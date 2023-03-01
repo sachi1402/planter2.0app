72373eb0d6669cd3485dcc35a3321075323aba7c
SSOURABH58 <ssourabh025@gamil.com>
2023-03-01 17:16:13 +0530

    .

---
+++ utils/TfConverter.js
@@ -0,0 +1,25 @@
+import * as tf from '@tensorflow/tfjs'
+import * as tfReact from '@tensorflow/tfjs-react-native'
+import * as FileSystem from 'expo-file-system'
+import '@tensorflow/tfjs-react-native'
+
+const options = { encoding: FileSystem.EncodingType.Base64 }
+
+// prettier-ignore
+const transformImageToTensor = async (uri, width, height) => {
+  await tf.ready()
+  console.log('\n [Running Converter process] \n')
+
+  // read the image as base64 and create buffer
+  const img64 = await FileSystem.readAsStringAsync(uri, options)
+  const imgBuffer = tf.util.encodeString(img64, 'base64').buffer
+  const raw = new Uint8Array(imgBuffer)
+
+  let imgTensor = tfReact.decodeJpeg(raw)
+
+  //resize the image
+  const img = tf.image.resizeNearestNeighbor(imgTensor, [width, height])
+  return img
+}
+
+export default transformImageToTensor
