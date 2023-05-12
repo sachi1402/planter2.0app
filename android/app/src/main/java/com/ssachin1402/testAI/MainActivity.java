4ff96a7f7b0cb40ba6554aad036f4052f8993b8e
SSOURABH58 <ssourabh025@gamil.com>
2023-05-12 14:56:32 +0530

    tried 🫤

--- android/app/src/main/java/com/ssachin1402/testAI/MainActivity.java
+++ android/app/src/main/java/com/ssachin1402/testAI/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends ReactActivity {
     // Set the theme to AppTheme BEFORE onCreate to support 
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
+    // setTheme(R.style.AppTheme);
     super.onCreate(null);
   }
 
@@ -34,17 +34,6 @@ public class MainActivity extends ReactActivity {
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
    * (aka React 18) with two boolean flags.
    */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        ));
-  }
 
   /**
    * Align the back button behavior with Android S
--- android/app/src/main/java/com/ssachin1402/testAI/MainApplication.java
+++ android/app/src/main/java/com/ssachin1402/testAI/MainApplication.java
@@ -12,8 +12,11 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 
-import expo.modules.ApplicationLifecycleDispatcher;
-import expo.modules.ReactNativeHostWrapper;
+//ReactNativeHostWrapper
+import com.facebook.react.ReactNativeHostWrapper;
+//ApplicationLifecycleDispatcher
+import com.facebook.react.bridge.ApplicationLifecycleDispatcher;
+
 
 import java.util.List;
 
@@ -21,10 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
     new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
-      @Override
-      public boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-      }
+
 
       @Override
       protected List<ReactPackage> getPackages() {
@@ -40,15 +40,6 @@ public class MainApplication extends Application implements ReactApplication {
         return "index";
       }
 
-      @Override
-      protected boolean isNewArchEnabled() {
-        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-      }
-
-      @Override
-      protected Boolean isHermesEnabled() {
-        return BuildConfig.IS_HERMES_ENABLED;
-      }
   });
 
   @Override
@@ -56,17 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
-    }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    ApplicationLifecycleDispatcher.onApplicationCreate(this);
-  }
 
   @Override
   public void onConfigurationChanged(@NonNull Configuration newConfig) {
